6285fd86e756390c2e4b2b412242c68a272250f8
arnabbis <biswas.anuska.2004@gmail.com>
2023-10-13 12:13:26 +0530

    feat: added files

--- longestSubString.js
+++ longestSubString.js
@@ -1,26 +1,41 @@
-let s = "pwwkew";
+let s = "nigzhtkqxr"
+
+;
 let store = [];
 let unique = [];
 let obj = {};
+let storeunique = [];
+let newEle = new Set()
 // let splitString ; 
 let sliceString=0;
 for(let i=0;i<s.length;i++){
-    for(j=i+1;j<s.length;j++){
-        s.slice(i,j)
-        store.push(s.slice(i,j))
+    for(j=i+1;j<=s.length;j++){
+        store.push(s.substring(i,j))
     }
 }
-// console.log(store)
+console.log(store)
 for(let i=0;i<store.length;i++){
+    // console.log("elements of the string",store[i]);
+    let hasDub = false;
     for(j=0;j<store[i].length;j++){
-        console.log([i][j])
-    if(obj[store[i][j]]===undefined){
-        obj[store[i][j]]=1
-    }
+        let ele = store[i][j]
+        // console.log("every charecter",ele)
+        if (store[i].indexOf(ele) !== store[i].lastIndexOf(ele)) {
+            hasDub = true;
+            break;
+        }
+}
+if(!hasDub){
+    storeunique.push(store[i])
 }
 }
-
-console.log(obj)
-
-
+console.log("store unique ele",storeunique)
+let storeLength = [];
+// console.log("sorted data",sort)
+for(let i=0;i<storeunique.length;i++){
+    storeLength.push(storeunique[i].length)
+}
+console.log(storeLength)
+let sortEleLength = storeLength.sort((a,b)=>a-b)
+console.log(sortEleLength[sortEleLength.length-1])
 
--- reverseInteger.js
+++ reverseInteger.js
@@ -1,11 +1,15 @@
 let data = 1534236469;
+const min32BitInteger = -Math.pow(2, 31);
+const max32BitInteger = Math.pow(2, 31) - 1;
+console.log(min32BitInteger);
+console.log(max32BitInteger)
 let convert = data.toString();
 let splitdata = convert.split('');
 let negetive = false;
-    if(splitdata[0]==='-'){
-        negetive = true;
-        splitdata = splitdata.splice(1);
-    }
+if(splitdata[0]==='-'){
+    negetive = true;
+    splitdata = splitdata.splice(1);
+}
 let string = '';
 for(let i=splitdata.length-1;i>=0;i--){
     string = string+splitdata[i]
@@ -15,5 +19,8 @@ if(negetive){
 }
 
 let convertNum = parseInt(string);
-console.log("convert to number",convertNum)
-//  9646324351
+if(convertNum<=  -min32BitInteger || convertNum>= max32BitInteger){
+    console.log("out of range")
+}else{
+    console.log(convertNum)
+}
