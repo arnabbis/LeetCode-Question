173f7620662944ba04a3f13a4471914c8d9dc6bc
arnabbis <biswas.anuska.2004@gmail.com>
2023-12-26 22:27:50 +0530

    feat: added files

--- countEqualDivisiblePair.js
+++ countEqualDivisiblePair.js
@@ -1,12 +1,12 @@
-let nums =[10,2,10,9,1,6,8,9,2,8];
-let k = 5;
+let nums = [3,1,2,2,2,1,3];
+let k = 2
 let store = [];
 for(let i=0;i<nums.length-1;i++){
     for(let j=i+1;j<nums.length;j++){
-        if(Math.abs(nums[i]-nums[j])===k){
+        if(nums[i]==nums[j] && (i*j)%k===0){
             store.push([i,j])
         }
     }
 }
-console.log(store)
+
 console.log(store.length)
--- countNoBadPairs.js
+++ countNoBadPairs.js
@@ -1,12 +1,11 @@
-let nums =[10,2,10,9,1,6,8,9,2,8];
-let k = 5;
+let nums = [1,2,3,4,5]
 let store = [];
 for(let i=0;i<nums.length-1;i++){
     for(let j=i+1;j<nums.length;j++){
-        if(Math.abs(nums[i]-nums[j])===k){
+        if(i<j && j-i!=nums[j]-nums[i]){
             store.push([i,j])
         }
     }
 }
-console.log(store)
+
 console.log(store.length)
